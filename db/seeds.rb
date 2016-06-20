58ce6baacf0ab68bdaaed3e5a2e1e61dc3e63742
Sonia <so.prevost@gmail.com>
2016-06-20 08:49:17 +0200

    seed-database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,55 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+  Curiosity.create(
+  name: 'Joli mug',
+  description: 'Reçu au Japon, lors d\'un congrès interlitières',
+  image_url: 'https://s-media-cache-ak0.pinimg.com/236x/4a/86/bf/4a86bfbf02b472e5b385762b8f267a91.jpg',
+  image_text: 'Un grand mug de lait pour bien commencer la journée'
+)
+
+Curiosity.create(
+  name: 'La bobine',
+  description: 'Meilleur jeu ever',
+  image_url: 'http://www.airoller.fr/cms/images/stories/articles/pelote_de_laine.jpg',
+  image_text: 'La bobine ! La bobine !'
+)
+
+Curiosity.create(
+  name: 'LE tee-shirt',
+  description: 'Il est sur ma liste pour Noel',
+  image_url: 'https://www.fifthsun.com/media/catalog/product/cache/1/image/1000x/a150d3402a4a7eeaf1a7d924d386d2d5/m/e/mens-stay-grumpy-grumpy-cat-t-shirt.jpg',
+  image_text: 'Vive moi'
+)
+
+Curiosity.create(
+  name: 'Le bonnet Lapin',
+  description: 'C\'est un cadeau pour mon beau frère, c\'est un siamois ... je n\'aime pas les siamois',
+  image_url: 'http://oddpad.com/wp-content/uploads/2015/11/cute-japanese-cat-scottish-fold-17-year-old-ura-1.jpg',
+  image_text: 'Je ne l\'aime pas'
+)
+
+  Curiosity.create(
+  name: 'Catopoly',
+  description: 'Dommage, on ne peut pas y jouer seul .. ',
+  image_url: 'https://s-media-cache-ak0.pinimg.com/236x/c9/f9/16/c9f91608fe96f1150b1f8a47c73a0f72.jpg',
+  image_text: 'Catopoly'
+)
+
+Curiosity.create(
+  name: 'Marque-page',
+  description: 'Il est beau.',
+  image_url: 'https://img1.etsystatic.com/103/1/7801041/il_214x170.843477875_7p24.jpg',
+  image_text: 'La prochaine fois que vous me croisez, vous saurez quoi faire .. '
+)
+
+  Curiosity.create(
+  name: 'Drago',
+  description: 'Mon meilleur ami',
+  image_url: 'https://pbs.twimg.com/media/CVLqAm8UsAA3loE.jpg',
+  image_text: 'Drago, mon ami'
+)
+
+Curiosity.create(
+  name: 'Tapis d\'entrée',
+  description: 'Devant la future chatière',
+  image_url: 'https://s-media-cache-ak0.pinimg.com/236x/a0/c7/82/a0c782481c4ba7c4511c7daa849d540b.jpg',
+  image_text: 'Tapis pour acceuillir mes potentiels visiteurs'
+)
